9c762f10289766afeb8229cab5caf9059d44d2c1
Mikhail-Kavzov <kavzovm@mail.ru>
2023-02-26 20:50:58 +0300

    add WebPage with WebView element

---
+++ RssReader/RssReader/Views/WebPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RssReader
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebPage : ContentPage
+    {
+
+        public WebPage(string url)
+        {
+            InitializeComponent();
+            webView.Source= new Uri(url);
+        }
+    }
+}
